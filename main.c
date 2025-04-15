6b99cbaa69552690be3748e7891db3adec5a66a1
b-minjun <a01076555033@gmail.com>
2025-04-15 23:51:16 +0900

    pwd,cd,함수분리리

--- main.c
+++ main.c
@@ -1,32 +1,83 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
+#include <dirent.h>
 
-char username[100];
-char password[100];
-char hostname[100];
-char prompt[100];
+#define MAX_INPUT_SIZE 4096
+
+char username[MAX_INPUT_SIZE];
+char password[MAX_INPUT_SIZE];
+char hostname[MAX_INPUT_SIZE];
+char input[MAX_INPUT_SIZE];
+char current_path[MAX_INPUT_SIZE];
+
+
+// get user information
+void get_user_info() {    
+    printf("Welcome to my Bash Shell!\n");
+
+    printf("Enter username: ");
+    fgets(username, sizeof(username), stdin);
+    username[strcspn(username, "\n")] = 0;
+
+    printf("Enter password: ");
+    fgets(password, sizeof(password), stdin);
+    password[strcspn(password, "\n")] = 0;
+
+    gethostname(hostname, sizeof(hostname));
+
+    getcwd(current_path, sizeof(current_path));
+}
+
+// pwd
+void pwd() {
+    char cwd[MAX_INPUT_SIZE];
+            if(getcwd(cwd, sizeof(cwd)) != NULL){// cwd : 성공 시 cwd의 주소 반환 / 실패 시 NULL 반환
+                printf("%s\n", cwd);
+            } else {
+                printf("pwd error\n");
+            }
+            return;
+}
+
+// cd
+void cd(char *input) {
+    char *path = input + 3;
+    if(chdir(path) != 0){// chdir : 성공 시 0 반환 / 실패 시 -1 반환
+        printf("cd error\n");
+    } else {
+        getcwd(current_path, sizeof(current_path));
+    }
+    return;
+}
 
 int main(){
     // get user information
-    printf("Welcome to my Bash Shell!\n");
-    printf("Enter username: ");
-    scanf("%s", username);
-    printf("Enter password: ");
-    scanf("%s", password);
-    gethostname(hostname, sizeof(hostname));
+    get_user_info();
 
     while(1){
-        // prompt
-        printf("%s@%s:~$ ", username, hostname);
-        scanf("%s", prompt);
+        // prompt 
+        printf("%s@%s:%s$ ", username, hostname, current_path);
+        fgets(input, MAX_INPUT_SIZE, stdin);
+        input[strcspn(input, "\n")] = 0;
 
         // exit
-        if(strcmp(prompt, "exit") == 0){
+        if(strcmp(input, "exit") == 0){
             printf("logout\n");
             break;
         }
+
+        // pwd
+        else if(strcmp(input, "pwd") == 0){
+            pwd();
+        }
+
+        // cd
+        else if(strncmp(input, "cd ", 3) == 0){
+            cd(input);
+        }
+
     }
     return 0;
-    
 }
