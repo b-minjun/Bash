bca0449917f842c7521031a7d1f3286ebe4b8ca4
b-minjun <a01076555033@gmail.com>
2025-04-16 13:11:00 +0900

    ls, cat, ~

--- main.c
+++ main.c
@@ -33,10 +33,10 @@ void get_user_info() {
 // pwd
 void pwd() {
     char cwd[MAX_INPUT_SIZE];
-            if(getcwd(cwd, sizeof(cwd)) != NULL){// cwd : 성공 시 cwd의 주소 반환 / 실패 시 NULL 반환
+            if(getcwd(cwd, sizeof(cwd)) != NULL){   // getcwd : 성공 시 cwd의 주소 반환 / 실패 시 NULL 반환
                 printf("%s\n", cwd);
             } else {
-                printf("pwd error\n");
+                perror("pwd error");
             }
             return;
 }
@@ -44,21 +44,65 @@ void pwd() {
 // cd
 void cd(char *input) {
     char *path = input + 3;
-    if(chdir(path) != 0){// chdir : 성공 시 0 반환 / 실패 시 -1 반환
-        printf("cd error\n");
-    } else {
+    if(chdir(path) == 0){   // chdir : 성공 시 0 반환 / 실패 시 -1 반환
         getcwd(current_path, sizeof(current_path));
+    } else {
+        perror("cd error");
     }
+    return;
+}
+
+// ls
+void ls(int show_all) {
+    DIR *dir;
+    struct dirent *entry;
+
+    dir = opendir(current_path);    // opendir : 성공 시 dir의 주소 반환 / 실패 시 NULL 반환
+    if (dir == NULL) {
+        perror("opendir");
+        return;
+    }
+
+    while ((entry = readdir(dir)) != NULL) {    // readdir : 성공 시 dirent 구조체의 주소 반환 / 실패 시 NULL 반환
+        if (show_all || entry->d_name[0] != '.') {  // show_all : 1이면 숨김 파일도 출력 / 0이면 숨김 파일 제외
+            printf("%s\n", entry->d_name);
+        }
+    }
+
+    closedir(dir);
+    return;
+}
+
+// cat
+void cat(char *input) {
+    char *filename = input + 4;
+    FILE *file = fopen(filename, "r");   // fopen : 성공 시 file의 주소 반환 / 실패 시 NULL 반환 / r : 읽기 전용 모드
+    if (file == NULL) {
+        perror("cat error");
+        return;
+    }
+    char c;
+    while ((c = fgetc(file)) != EOF) {  // fgetc : 성공 시 문자 반환 / 실패 시 EOF 반환
+        putchar(c);
+    }
+    fclose(file);
+    printf("\n");   
     return;
 }
 
 int main(){
     // get user information
     get_user_info();
+    
 
     while(1){
         // prompt 
-        printf("%s@%s:%s$ ", username, hostname, current_path);
+        const char *home = getenv("HOME");
+        if(strncmp(current_path, home, strlen(home)) == 0){ // home directory = '~'
+            printf("%s@%s:~%s$ ", username, hostname, current_path + strlen(home));
+        } else{
+            printf("%s@%s:%s$ ", username, hostname, current_path);
+        }
         fgets(input, MAX_INPUT_SIZE, stdin);
         input[strcspn(input, "\n")] = 0;
 
@@ -78,6 +122,19 @@ int main(){
             cd(input);
         }
 
+        // ls
+        else if(strncmp(input, "ls", 2) == 0){
+            if(strstr(input, "-a")){
+                ls(1);
+            } else{
+                ls(0);
+            }
+        }
+
+        // cat
+        else if(strncmp(input, "cat ", 4) == 0){
+            cat(input);
+        }
     }
     return 0;
 }
